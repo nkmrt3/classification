14348bea6f15b0c4c2843aa9a39d881b78216f04
dingwalnitin <90894005+dingwalnitin@users.noreply.github.com>
2023-09-15 23:52:13 +0530

    com

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import pandas as pd
 import tensorflow as tf
@@ -47,4 +48,6 @@ def predict():
         return render_template('index.html', prediction=prediction)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Use the PORT environment variable if available, or default to 8080
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=True)
