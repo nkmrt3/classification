4c44094272b8c791e1233ce1eaa63ba35d360fdf
dingwalnitin <90894005+dingwalnitin@users.noreply.github.com>
2023-09-16 01:21:27 +0530

    restore

--- app.py
+++ app.py
@@ -4,47 +4,25 @@ import tensorflow as tf
 import tensorflow_hub as hub
 from flask import Flask, render_template, request
 
+# Initialize Flask app
 app = Flask(__name__)
 
-# Load your pre-trained model and Hub layer
-embedding = "https://tfhub.dev/google/nnlm-en-dim50/2"
-hub_layer = hub.KerasLayer(embedding, dtype=tf.string, trainable=True)
-model = tf.keras.models.load_model('wine_points_model')
+# Load the saved model
+model = tf.keras.models.load_model("wine_points_model")  # Update with the correct path
 
-# Function to preprocess input text
-def preprocess_input(description):
-    description = [description]
-    description = hub_layer(description)
-    return description
+# Function to preprocess the input text and make predictions
+def predict_wine_review(text):
+    text = [text]  # Convert input text to a list
+    prediction = model.predict(text)[0][0]
+    return "Good" if prediction >= 0.5 else "Bad"
 
-# Define the home route
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-# Define the predict route
-@app.route('/predict', methods=['POST'])
-def predict():
+@app.route('/', methods=['GET', 'POST'])
+def index():
     if request.method == 'POST':
-        # Get the wine description from the form
-        wine_description = request.form['wine_description']
-
-        # Preprocess the input
-        preprocessed_input = preprocess_input(wine_description)
-
-        # Make predictions using the model
-        predicted_prob = model.predict(preprocessed_input)[0][0]
-
-        # Determine the label based on a threshold (e.g., 0.5)
-        predicted_label = 1 if predicted_prob >= 0.5 else 0
-
-        # Prepare the prediction result to pass to the template
-        prediction = {
-            'probability': predicted_prob,
-            'label': 'Yes' if predicted_label == 1 else 'No'
-        }
-
-        return render_template('index.html', prediction=prediction)
+        review = request.form['review']
+        prediction = predict_wine_review(review)
+        return render_template('index.html', review=review, prediction=prediction)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
